39979b4f71e86e3d74363a85ea66a4ef0d26a2ba
ktjisebojtioeskotbwioi <nemlopottnevxd@gmail.com>
2023-05-27 13:43:39 +0200

    dawdawsd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 
 
-namespace xddddd
+namespace kajarendeloapp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- userClassLib.cs
+++ userClassLib.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace xddddd
+namespace kajarendeloapp
 {
     //felhasználó class
     public class User
